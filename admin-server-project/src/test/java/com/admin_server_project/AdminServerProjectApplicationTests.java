42dd3f54f304d4eaff92e8588e8489beb21efc0d
asheesh859 <ak2305@checkboxtechnology.com>
2025-07-21 16:38:32 +0530

    add admin server in project 21/07/2024 4:38 PM

---
+++ admin-server-project/src/test/java/com/admin_server_project/AdminServerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.admin_server_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
